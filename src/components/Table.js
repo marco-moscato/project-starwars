5182e7865a157f6ecb3944d231be2707a03f4c84
Marco Moscato <marcomoscatto@gmail.com>
2023-01-28 18:20:47 -0300

    feat: renderiza a tabela de planetas
    renderiza a tabela de planetas com as informações em cada célula. Falta criar lógica para exlucir a chave residents antes de salvar no estado.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import fetchPlanetsAPI from '../services/fetchPlanetsAPI';
+import './Table.css';
 
 export default function Table() {
   const [planets, setPlanets] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [planetsKeys, setPlanetsKeys] = useState([]);
 
   // Falta retirar a chave residents do array;
   useEffect(
@@ -14,6 +16,7 @@ export default function Table() {
         // const planetsArray = [...response];
         // const filterPlanets = response.map((planet) => planet.residents);
           setPlanets(response);
+          setPlanetsKeys(Object.keys(response[0]));
           setLoading(false);
         });
     },
@@ -23,15 +26,39 @@ export default function Table() {
   return (
     <div>
       { loading && <h3>...LOADING...</h3> }
-      <table>
+      <table className="planetsTable">
         <thead>
           <tr>
-            <th>
-              { planets.map((planet) => Object.keys(planet)) }
-            </th>
+            {/* Refatorar para evitar de usar o state */}
+            { planetsKeys.map((keys) => <th key={ keys }>{ keys }</th>)}
           </tr>
         </thead>
+        <tbody>
+          {/* Refatorar esse map para reduzir a quantidade de linhas */}
+          { planets.map((planet) => (
+            <tr key={ planet }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.residents }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
 }
+
+// Table.propTypes = {
+//   keys
+// }
