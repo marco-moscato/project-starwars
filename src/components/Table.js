7f6bf2b52b86e0e922a58f7d12bf1706f3e0a488
Marco Moscato <marcomoscatto@gmail.com>
2023-01-28 15:37:01 -0300

    feat: renderizando a table head com a chave da API.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,17 +3,31 @@ import fetchPlanetsAPI from '../services/fetchPlanetsAPI';
 
 export default function Table() {
   const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => async () => fetchPlanetsAPI()
-    .then((response) => setPlanets(response)), []);
+  // Falta retirar a chave residents do array;
+  useEffect(
+    () => {
+      setLoading(true);
+      fetchPlanetsAPI()
+        .then((response) => {
+        // const planetsArray = [...response];
+        // const filterPlanets = response.map((planet) => planet.residents);
+          setPlanets(response);
+          setLoading(false);
+        });
+    },
+    [],
+  );
 
   return (
     <div>
+      { loading && <h3>...LOADING...</h3> }
       <table>
         <thead>
           <tr>
             <th>
-              {/* { planets } */}
+              { planets.map((planet) => Object.keys(planet)) }
             </th>
           </tr>
         </thead>
