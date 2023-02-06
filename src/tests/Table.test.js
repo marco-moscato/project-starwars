47c370887e0d517d5dd0771f2aabee1cc98bc69c
Marco Moscato <marcomoscatto@gmail.com>
2023-02-06 14:49:40 -0300

    feat: parte dos testes da tabela.

--- src/tests/Table.test.js
+++ src/tests/Table.test.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('Test table content', () => {
-  render(<App />);
-  const table = screen.getByRole('table');
-  const headerRow = screen.getAllByRole('rowgroup');
-  const rowHeader = screen.getByRole('rowheader');
-  const rows = screen.getAllByRole('row');
+describe('Test table components', () => {
+    test('If there is a table content when rendering and its size', () => {
+        render(<App />);
+        
+        const table = screen.getByRole('table');
+        const header = screen.getAllByRole('columnheader');
+        const rows = screen.getAllByRole('row');
 
-  expect(table).toBeInTheDocument();
-//   expect(rows.length).toBe(11);
-  expect(rowHeader).toHaveLength(13);
-//   expect(table).toContainElement()
-//   expect(tableHeaders).toContainElement('th');
-  
-});
+        expect(table).toBeDefined();
+        expect(header).toHaveLength(13);
+        // expect(rows).toHaveLength(10);
+    });
+    test('if all filters are being on screen', () => {
+        render(<App />);
+        
+        const table = screen.getByRole('table');
+        const header = screen.getAllByRole('columnheader');
+        const rows = screen.getAllByRole('row');
+
+        expect(table).toBeDefined();
+        expect(header).toHaveLength(13);
+        // expect(rows).toHaveLength(10);
+    });
+})
